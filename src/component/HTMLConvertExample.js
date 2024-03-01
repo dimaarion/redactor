9c677b6e77fe88e3fb758a67c43a906efcb441fd
dimaarion <dimaarion@gmail.com>
2024-03-01 22:56:11 +0200

    dimaarion/redactor

---
+++ src/component/HTMLConvertExample.js
@@ -0,0 +1,155 @@
+import {
+    convertFromRaw,
+    convertToRaw,
+    CompositeDecorator,
+    Editor,
+    ContentState,
+    EditorState, convertFromHTML
+} from 'draft-js';
+import React, {useEffect, useRef} from "react";
+
+
+export default function HTMLConvertExample() {
+
+    const [editorState, setEditorState] = React.useState(
+        () => EditorState.createEmpty(),
+    );
+
+    const editor = useRef(null);
+
+    const focus = () => {
+        if (editor.current) editor.current.focus();
+    };
+
+
+    const logState = () => {
+        const content = editorState.getCurrentContent();
+        console.log(convertToRaw(content));
+    };
+
+
+    const Link = (props) => {
+        const {url} = props.contentState.getEntity(props.entityKey).getData();
+        return (
+            <a href={url} style={styles.link}>
+                {props.children}
+            </a>
+        );
+    };
+
+
+    function findImageEntities(contentBlock, callback, contentState) {
+        contentBlock.findEntityRanges(
+            (character) => {
+                const entityKey = character.getEntity();
+                return (
+                    entityKey !== null &&
+                    contentState.getEntity(entityKey).getType() === 'IMAGE'
+                );
+            },
+            callback
+        );
+    }
+
+    const Image = (props) => {
+        const {
+            height,
+            src,
+            width,
+        } = props.contentState.getEntity(props.entityKey).getData();
+
+
+        const decorator = new CompositeDecorator([
+            {
+                strategy: findLinkEntities,
+                component: Link,
+            },
+            {
+                strategy: findImageEntities,
+                component: Image,
+            },
+        ]);
+
+
+
+        const sampleMarkup =
+            '<b>Bold text</b>, <i>Italic text</i><br/ ><br />' +
+            '<a href="http://www.facebook.com">Example link</a><br /><br/ >' +
+            '<img src="image.png" height="112" width="200" />';
+
+        const blocksFromHTML = convertFromHTML(sampleMarkup);
+        const state = ContentState.createFromBlockArray(
+            blocksFromHTML.contentBlocks,
+            blocksFromHTML.entityMap,
+        );
+
+
+
+        function findLinkEntities(contentBlock, callback, contentState) {
+            contentBlock.findEntityRanges(
+                (character) => {
+                    const entityKey = character.getEntity();
+                    return (
+                        entityKey !== null &&
+                        contentState.getEntity(entityKey).getType() === 'LINK'
+                    );
+                },
+                callback
+            );
+        }
+
+
+        return (
+            <img src={src} height={height} width={width}/>
+        );
+    };
+
+    useEffect(() => {
+        setEditorState(EditorState.createWithContent(
+            state,
+            decorator,
+        ),)
+
+
+    }, [])
+   console.log('blo')
+
+    return <>
+        <div style={styles.root}>
+            <div style={{marginBottom: 10}}>
+                Sample HTML converted into Draft content state
+            </div>
+            <div style={styles.editor} onClick={focus}>
+                <Editor
+                    editorState={editorState}
+                    onChange={setEditorState}
+                    ref={editor.current}
+                />
+            </div>
+            <input
+                onClick={logState}
+                style={styles.button}
+                type="button"
+                value="Log State"
+            />
+        </div>
+    </>
+}
+
+const styles = {
+    root: {
+        fontFamily: '\'Helvetica\', sans-serif',
+        padding: 20,
+        width: 600,
+    },
+    editor: {
+        border: '1px solid #ccc',
+        cursor: 'text',
+        minHeight: 80,
+        padding: 10,
+    },
+    button: {
+        marginTop: 10,
+        textAlign: 'center',
+    },
+};

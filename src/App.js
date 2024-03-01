36cd2e355586a558a21817868d586c83561aa7d6
dimaarion <dimaarion@gmail.com>
2024-03-01 22:55:49 +0200

    dimaarion/redactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'draft-js/dist/Draft.css';
 import EditorRedactor from "./component/EditorRedactor";
+import HTMLConvertExample from "./component/HTMLConvertExample";
 
 
 function App() {
 
-    return <EditorRedactor/>;
+    return <HTMLConvertExample />;
 }
 
 export default App;
---
+++ src/component/EntityEditorExample.js
@@ -0,0 +1,9 @@
+import { convertFromRaw,
+    convertToRaw,
+    CompositeDecorator,
+    Editor,
+    EditorState} from 'draft-js';
+
+export default function EntityEditorExample(props){
+    return <></>
+}
